100bd7bb57600bd8743d349275e25428f5e24240
otarza <otar.zakalashvili@gmail.com>
2016-02-15 13:22:57 +0400

    path_alias_processor sevice implemented This service alters inbount path according to #1

--- src/AliasPathProcessor.php
+++ src/AliasPathProcessor.php
@@ -10,12 +10,17 @@ use Drupal\Core\PathProcessor\InboundPathProcessorInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * Class AliasPathProcessor.
+ *
+ * @package Drupal\entity_alias
+ */
 class AliasPathProcessor implements InboundPathProcessorInterface {
 
   /**
    * Processes the inbound path.
    *
-   * @param string                                    $path
+   * @param string $path
    *   The path to process, with a leading slash.
    * @param \Symfony\Component\HttpFoundation\Request $request
    *   The HttpRequest object representing the current request.
@@ -24,22 +29,20 @@ class AliasPathProcessor implements InboundPathProcessorInterface {
    *   The processed path.
    */
   public function processInbound($path, Request $request) {
-    $redirect    = FALSE;
-    $parsed_path = parse_url($path);
 
-    if (isset($parsed_path['path'])) {
-      $pieces = explode('-', $parsed_path['path']);
-      $id     = end($pieces);
-
-      if (is_numeric($id)) {
-        $alias = \Drupal::service('path.alias_manager')
-          ->getAliasByPath("/node/" . $id);
-
-        if ($path != $alias) {
-          $response = new RedirectResponse($alias);
-          $response->send();
-          }
+    $request_uri = $request->getRequestUri();
+    $pieces      = explode('-', $request_uri);
+    $id          = end($pieces);
+    if (is_numeric($id)) {
+      $alias = \Drupal::service('path.alias_manager')
+        ->getAliasByPath("/node/" . $id);
+      if ($path != $alias) {
+        $response = new RedirectResponse($alias);
+        $response->send();
       }
     }
+
+    return $path;
   }
+
 }
