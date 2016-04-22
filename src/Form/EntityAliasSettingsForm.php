86149ed84756ac3dc28f65579ff9b764fccc54ee
Lasha <lashab@picktek.com>
2016-04-22 11:32:49 +0000

    Configuration form router and description #3

--- src/Form/EntityAliasSettingsForm.php
+++ src/Form/EntityAliasSettingsForm.php
@@ -9,6 +9,7 @@ namespace Drupal\entity_alias\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
 
 /**
  * Class EntityAliasSettingsForm.
@@ -39,13 +40,15 @@ class EntityAliasSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $content_types           = \Drupal::service('entity.manager')
+    $content_types = \Drupal::service('entity.manager')
       ->getStorage('node_type')
       ->loadMultiple();
-    $config                  = $this->config('entity_alias.settings');
+    $config = $this->config('entity_alias.settings');
     $configured_entity_types = $config->get('entity_types');
 
-    $form[]['#prefix'] = t('<p> Content types checked here will be smart aliased.</p>');
+    $form[]['#prefix'] = t('<p>Smart alias uses @pathauto</p><p>Content types checked here will be smart aliased.</p>', [
+      '@pathauto' => \Drupal::l('pathauto', Url::fromRoute('pathauto.settings.form')),
+    ]);
 
     foreach ($content_types as $type_id => $content_type) {
       $default_value = 0;
@@ -54,8 +57,8 @@ class EntityAliasSettingsForm extends ConfigFormBase {
       }
 
       $form[$type_id] = array(
-        '#type'          => 'checkbox',
-        '#title'         => $content_type->label(),
+        '#type' => 'checkbox',
+        '#title' => $content_type->label(),
         '#default_value' => $default_value,
       );
     }
@@ -82,14 +85,14 @@ class EntityAliasSettingsForm extends ConfigFormBase {
       ->getStorage('node_type')
       ->loadMultiple();
 
-    $config           = \Drupal::service('config.factory')
+    $config = \Drupal::service('config.factory')
       ->getEditable('entity_alias.settings');
     $submitted_values = [];
 
     foreach ($content_types as $type_id => $content_type) {
       $submitted_values[$type_id] = [
         'entity_type' => $type_id,
-        'enabled'     => $form_state->getValue($type_id),
+        'enabled' => $form_state->getValue($type_id),
       ];
     }
 
